7342f7f764e55d5b87626e6bcf3fe819dfdddd9b
oeresundsgruppen <peterholm@gmx.net>
2016-08-14 16:10:01 +0200

    test

---
+++ header.js
@@ -0,0 +1,19 @@
+document.write('\
+  <tr>\
+    <td colspan="2" bgcolor="#152ca1"><img src="images/top.gif" alt="ejerforeningen &oslash;resund strandpark, etape ii" width="100%" height="77" /></td>\
+  </tr>\
+  <tr>\
+    <td class="menu" width="185" valign="top" bgcolor="#1935c5"><p>&nbsp;</p>\
+    <p><a href="index.html"><img src="images/forside_on.gif" alt="forside" width="175" height="25" border="0" /></a><br />\
+      <a href="ejerforeningen.html"><img src="images/ejerforeningen.gif" alt="ejerforeningen" width="175" height="25" border="0" /></a><br />\
+      <a href="referater.html"><img src="images/referater.gif" alt="referater, bestyrelsesm&oslash;der" width="175" height="25" border="0" /></a><br />\
+      <a href="bestyrelsen.html"><img src="images/bestyrelsen.gif" alt="bestyrelsen" width="175" height="25" border="0" /></a><br />\
+      <a href="husorden.html"><img src="images/husorden.gif" alt="husorden" width="175" height="25" border="0" /></a><br />\
+      <a href="vedtaegter.html"><img src="images/vedtaegter.gif" alt="vedt&aelig;gter" width="175" height="25" border="0" /></a><br />\
+      <a href="generalforsamling.html"><img src="images/generalforsamling.gif" alt="generalforsamling" width="175" height="25" border="0" /></a><br />\
+      <a href="grundejerforening.html"><img src="images/grundejerforening.gif" alt="grundejerforeningen" width="175" height="25" border="0" /></a><br />\
+      <a href="svar.html"><img src="images/svar.gif" alt="f&aring; svar p&aring; dine sp&oslash;rgsm&aring;l" width="175" height="25" border="0" /></a> <br />\
+      <a href="kontakter.html"><img src="images/kontakter.gif" alt="kontakter" width="175" height="25" border="0" /></a><br />\
+      <a href="links.html"><img src="images/links.gif" alt="nyttige links" width="175" height="25" border="0" /></a></p>\
+    </td>\
+');
