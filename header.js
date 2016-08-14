16faf85c68f88ce1d01e712474e93df3e91f7e4d
oeresundsgruppen <peterholm@gmx.net>
2016-08-15 00:05:41 +0200

    links2

--- header.js
+++ header.js
@@ -4,16 +4,16 @@ document.write('\
   </tr>\
   <tr>\
     <td class="menu" width="185" valign="top" bgcolor="#1935c5"><p>&nbsp;</p>\
-    <p><a href="index.html"><img src="images/forside_on.gif" alt="forside" width="175" height="25" border="0" /></a><br />\
+    <p><a href="index"><img src="images/forside_on.gif" alt="forside" width="175" height="25" border="0" /></a><br />\
       <a href="ejerforeningen"><img src="images/ejerforeningen.gif" alt="ejerforeningen" width="175" height="25" border="0" /></a><br />\
-      <a href="referater.html"><img src="images/referater.gif" alt="referater, bestyrelsesm&oslash;der" width="175" height="25" border="0" /></a><br />\
-      <a href="bestyrelsen.html"><img src="images/bestyrelsen.gif" alt="bestyrelsen" width="175" height="25" border="0" /></a><br />\
-      <a href="husorden.html"><img src="images/husorden.gif" alt="husorden" width="175" height="25" border="0" /></a><br />\
-      <a href="vedtaegter.html"><img src="images/vedtaegter.gif" alt="vedt&aelig;gter" width="175" height="25" border="0" /></a><br />\
-      <a href="generalforsamling.html"><img src="images/generalforsamling.gif" alt="generalforsamling" width="175" height="25" border="0" /></a><br />\
-      <a href="grundejerforening.html"><img src="images/grundejerforening.gif" alt="grundejerforeningen" width="175" height="25" border="0" /></a><br />\
-      <a href="svar.html"><img src="images/svar.gif" alt="f&aring; svar p&aring; dine sp&oslash;rgsm&aring;l" width="175" height="25" border="0" /></a> <br />\
-      <a href="kontakter.html"><img src="images/kontakter.gif" alt="kontakter" width="175" height="25" border="0" /></a><br />\
-      <a href="links.html"><img src="images/links.gif" alt="nyttige links" width="175" height="25" border="0" /></a></p>\
+      <a href="referater"><img src="images/referater.gif" alt="referater, bestyrelsesm&oslash;der" width="175" height="25" border="0" /></a><br />\
+      <a href="bestyrelsen"><img src="images/bestyrelsen.gif" alt="bestyrelsen" width="175" height="25" border="0" /></a><br />\
+      <a href="husorden"><img src="images/husorden.gif" alt="husorden" width="175" height="25" border="0" /></a><br />\
+      <a href="vedtaegter"><img src="images/vedtaegter.gif" alt="vedt&aelig;gter" width="175" height="25" border="0" /></a><br />\
+      <a href="generalforsamling"><img src="images/generalforsamling.gif" alt="generalforsamling" width="175" height="25" border="0" /></a><br />\
+      <a href="grundejerforening"><img src="images/grundejerforening.gif" alt="grundejerforeningen" width="175" height="25" border="0" /></a><br />\
+      <a href="svar"><img src="images/svar.gif" alt="f&aring; svar p&aring; dine sp&oslash;rgsm&aring;l" width="175" height="25" border="0" /></a> <br />\
+      <a href="kontakter"><img src="images/kontakter.gif" alt="kontakter" width="175" height="25" border="0" /></a><br />\
+      <a href="links"><img src="images/links.gif" alt="nyttige links" width="175" height="25" border="0" /></a></p>\
     </td>\
 ');
