1f733bea2e706216b29a62943981dd1569598522
Peter Holm <peterholm@gmx.net>
2019-01-16 23:55:29 +0100

    menu

--- header.js
+++ header.js
@@ -4,17 +4,17 @@ document.write('\
   </tr>\
   <tr>\
     <td class="menu" width="185" valign="top" bgcolor="#1935c5"><p>&nbsp;</p>\
-    <p><a href="index"><img src="images/forside_on.gif" alt="forside" width="175" height="25" border="0" /></a><br />\
-      <a href="ejerforeningen"><img src="images/ejerforeningen.gif" alt="ejerforeningen" width="175" height="25" border="0" /></a><br />\
-      <a href="referater"><img src="images/referater.gif" alt="referater, bestyrelsesm&oslash;der" width="175" height="25" border="0" /></a><br />\
-      <a href="bestyrelsen"><img src="images/bestyrelsen.gif" alt="bestyrelsen" width="175" height="25" border="0" /></a><br />\
-      <a href="husorden"><img src="images/husorden.gif" alt="husorden" width="175" height="25" border="0" /></a><br />\
-      <a href="vedtaegter"><img src="images/vedtaegter.gif" alt="vedt&aelig;gter" width="175" height="25" border="0" /></a><br />\
-      <a href="generalforsamling"><img src="images/generalforsamling.gif" alt="generalforsamling" width="175" height="25" border="0" /></a><br />\
-      <a href="regnskaber"><img src="images/regnskaber.gif" alt="regnskaber" width="175" height="25" border="0" /></a><br />\
-      <a href="grundejerforening"><img src="images/grundejerforening.gif" alt="grundejerforeningen" width="175" height="25" border="0" /></a><br />\
-      <a href="svar"><img src="images/svar.gif" alt="f&aring; svar p&aring; dine sp&oslash;rgsm&aring;l" width="175" height="25" border="0" /></a> <br />\
-      <a href="kontakter"><img src="images/kontakter.gif" alt="kontakter" width="175" height="25" border="0" /></a><br />\
-      <a href="links"><img src="images/links.gif" alt="nyttige links" width="175" height="25" border="0" /></a></p>\
+    <p><a href="index"><div class="menu1">Forsiden</div></a><br />\
+      <a href="ejerforeningen"><div class="menu1">Ejerforeningen</div></a><br />\
+      <a href="referater"><div class="menu2">Referater fra bestyrelsesmøder</div></a><br />\
+      <a href="bestyrelsen"><div class="menu2">Bestyrelsen</div></a><br />\
+      <a href="husorden"><div class="menu2">Husorden</div></a><br />\
+      <a href="vedtaegter"><div class="menu2">Vedtægter</div></a><br />\
+      <a href="generalforsamling"><div class="menu2">Generalforsamling</div></a><br />\
+      <a href="regnskaber"><div class="menu2">Regnskaber</div></a><br />\
+      <a href="grundejerforening"><div class="menu2">Grundejerforeningen</div></a><br />\
+      <a href="svar"><div class="menu1">Få svar på dine spørgsmål</div></a> <br />\
+      <a href="kontakter"><div class="menu1">Kontakter</div></a><br />\
+      <a href="links"><div class="menu1">Nyttige Links</div></a></p>\
     </td>\
 ');
