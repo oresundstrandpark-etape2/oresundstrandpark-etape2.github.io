4ec10a9762781550f4ce7156c4c39704fa2c9fd6
Peter Holm <peterholm@gmx.net>
2019-01-17 00:16:43 +0100

    menu2

--- header.js
+++ header.js
@@ -4,17 +4,17 @@ document.write('\
   </tr>\
   <tr>\
     <td class="menu" width="185" valign="top" bgcolor="#1935c5"><p>&nbsp;</p>\
-    <p><a href="index"><div class="menu1">Nyheder</div></a><br />\
-      <a href="ejerforeningen"><div class="menu1">Ejerforeningen</div></a><br />\
-      <a href="referater"><div class="menu2">Referater fra bestyrelsesmøder</div></a><br />\
-      <a href="bestyrelsen"><div class="menu2">Bestyrelsen</div></a><br />\
-      <a href="husorden"><div class="menu2">Husorden</div></a><br />\
-      <a href="vedtaegter"><div class="menu2">Vedtægter</div></a><br />\
-      <a href="generalforsamling"><div class="menu2">Generalforsamling</div></a><br />\
-      <a href="regnskaber"><div class="menu2">Regnskaber</div></a><br />\
-      <a href="grundejerforening"><div class="menu2">Grundejerforeningen</div></a><br />\
-      <a href="svar"><div class="menu1">Typiske spørgsmål</div></a> <br />\
-      <a href="kontakter"><div class="menu1">Kontakter</div></a><br />\
+    <p><a href="index"><div class="menu1">Nyheder</div></a><br/>\
+      <a href="ejerforeningen"><div class="menu1">Ejerforeningen</div></a><br/>\
+      <a href="referater"><div class="menu2">Referater fra bestyrelsesmøder</div></a><br/>\
+      <a href="bestyrelsen"><div class="menu2">Bestyrelsen</div></a><br/>\
+      <a href="husorden"><div class="menu2">Husorden</div></a><br/>\
+      <a href="vedtaegter"><div class="menu2">Vedtægter</div></a><br/>\
+      <a href="generalforsamling"><div class="menu2">Generalforsamling</div></a><br/>\
+      <a href="regnskaber"><div class="menu2">Regnskaber</div></a><br/>\
+      <a href="grundejerforening"><div class="menu2">Grundejerforeningen</div></a><br/>\
+      <a href="svar"><div class="menu1">Typiske spørgsmål</div></a><br/>\
+      <a href="kontakter"><div class="menu1">Kontakter</div></a><br/>\
       <a href="links"><div class="menu1">Nyttige Links</div></a></p>\
     </td>\
 ');
